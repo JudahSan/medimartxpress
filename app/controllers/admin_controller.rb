02d9d8cbb287ea26e6d1fd46cd29a4542b457bce
JudahSan <baraka.judah@yahoo.com>
2024-04-05 02:03:54 +0300

    Add store visual analytics.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -8,13 +8,13 @@ class AdminController < ApplicationController
     @orders = Order.where(fulfilled: false).order(created_at: :desc).take(5)
     @quick_stats = {
       sales: Order.where(created_at: Time.now.midnight..Time.now).count,
-      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total).round(),
-      average_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total).round(),
+      revenue: Order.where(created_at: Time.now.midnight..Time.now).sum(:total)&.round(),
+      average_sale: Order.where(created_at: Time.now.midnight..Time.now).average(:total)&.round(),
       pre_sale: OrderProduct.joins(:order).where(orders: { created_at: Time.now.midnight..Time.now }).average(:quantity)
     }
     @orders_by_day = Order.where('created_at > ?', Time.now - 7.days).order(:created_at)
     @orders_by_day = @orders_by_day.group_by { |order| order.created_at.to_date }
-    @revenue_by_day = @orders_by_day.map { |day, orders| [day.strftime("%A"), orders.sum(&:total)] }
+    @revenue_by_day = @orders_by_day.map { |day, orders| [day.strftime("%A"), orders.sum(&:total)] }.to_h
     if @revenue_by_day.count < 7
       days_of_week = %w[Monday Tuesday Wednesday Thursday Friday Saturday Sunday]
 
@@ -27,7 +27,7 @@ class AdminController < ApplicationController
       ordered_days_with_current_last = days_of_week[next_day_index..-1] + days_of_week[0...next_day_index]
 
       complete_ordered_array_with_current_last = ordered_days_with_current_last.map { |day| [day, data_hash.fetch(day, 0)] }
-      @revenue_by_day = complete_ordered_array_with_current_last.to_h
+      @revenue_by_day = complete_ordered_array_with_current_last
     end
   end
 end
