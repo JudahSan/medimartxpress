ce1ec5f9753978d271a8b25cbfe94a97f76672ee
JudahSan <baraka.judah@gmail.com>
2024-09-15 02:33:53 +0300

    feat: add admin product page pagination

--- config/initializers/pagy.rb
+++ config/initializers/pagy.rb
@@ -2,5 +2,6 @@
 Pagy::DEFAULT[:limit] = 10 # items per page
 Pagy::DEFAULT[:size]  = 9  # nav bar links
 # Better user experience handled automatically
+require 'pagy/extras/countless'
 require 'pagy/extras/overflow'
 Pagy::DEFAULT[:overflow] = :last_page
