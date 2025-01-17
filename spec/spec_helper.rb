5d5109a6cae712eb3a26719b14e48d78872d4f22
judahsan <baraka.judah@gmail.com>
2025-01-17 11:37:53 +0300

    Fix NoMethodError related to fixture_path in RSpec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -18,6 +18,8 @@ require "factory_bot_rails"
 #
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
+  # define fixed_path
+  config.fixture_path = Rails.root.join("spec/fixtures").to_s
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
